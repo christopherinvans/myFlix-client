aba3ca5d5f97978ef4638a10ebfd82505ec4d293
christopherinvans <106615358+christopherinvans@users.noreply.github.com>
2022-11-04 10:21:19 -0700

    update components

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,13 +7,21 @@ class MainView extends React.Component {
     constructor(){
         super();
         this.state = {
-          movies: [
-            { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
-            { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
-            { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
-          ],
+          movies: [],
           selectedMovie: null
         }
+      }
+
+      componentDidMount(){
+        axios.get('https://enigmatic-river-99618.herokuapp.com/movies')
+          .then(response => {
+            this.setState({
+              movies: response.data
+            });
+          })
+          .catch(error => {
+            console.log(error);
+          });
       }
 
       setSelectedMovie(newSelectedMovie) {
@@ -31,10 +39,16 @@ class MainView extends React.Component {
       
         return (
           <div className="main-view">
-            {movies.map(movie => <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setState({ selectedMovie: newSelectedMovie }); }} />)}
+            {selectedMovie
+              ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+              : movies.map(movie => (
+                <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+             ))
+            }
           </div>
         );
       }
-}
+ }
+
 
 export default MainView;
