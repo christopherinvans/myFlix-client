82ea32b4ac34d82d1a73c1cbdc7fbeb6bff3576b
christopherinvans <106615358+christopherinvans@users.noreply.github.com>
2022-12-21 20:26:54 -0800

    debugged console errors

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,135 +1,421 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
 
-import './main-view.scss';
+// import './main-view.scss';
+
+// import { RegistrationView } from '../registration-view/registration-view';
+// import { LoginView } from '../login-view/login-view';
+// import { MovieCard } from "../movie-card/movie-card";
+// import { MovieView } from "../movie-view/movie-view";
+
+// class MainView extends React.Component {
+
+//     constructor(){
+//         super();
+//         this.state = {
+//           movies: [],
+//           selectedMovie: null,
+//         }
+//       }
+
+//       componentDidMount(){
+//         axios.get('https://enigmatic-river-99618.herokuapp.com/movies')
+//           .then(response => {
+//             this.setState({
+//               movies: response.data
+//             });
+//           })
+//           .catch(error => {
+//             console.log(error);
+//           });
+//       }
+
+//       setSelectedMovie(newSelectedMovie) {
+//         this.setState({
+//           selectedMovie: newSelectedMovie
+//         });
+//       }
+
+//       onLoggedIn(authData) {
+//         console.log(authData);
+//         this.setState({
+//           user: authData.user.Username
+//         });
+      
+//         localStorage.setItem('token', authData.token);
+//         localStorage.setItem('user', authData.user.Username);
+//         this.getMovies(authData.token);
+//       }
+
+//       registration(registered) {
+//         this.setState({
+//           registered,
+//         });
+//       }
+
+//       getMovies(token) {
+//         axios.get('https://enigmatic-river-99618.herokuapp.com/movies', {
+//           headers: { Authorization: `Bearer ${token}`}
+//         })
+//         .then(response => {
+//           // Assign the result to the state
+//           this.setState({
+//             movies: response.data
+//           });
+//         })
+//         .catch(function (error) {
+//           console.log(error);
+//         });
+//       }
+
+//       render() {
+//         // const { movies, selectedMovie, registered } = this.state;
+//         const storedUser = JSON.parse(localStorage.getItem("user"));
+//         const storedToken = localStorage.getItem("token");
+//         const [user, setUser] = useState(storedUser? storedUser : null);
+//         const [token, setToken] = useState(storedToken? storedToken : null);
+//         const [movies, setMovies] = useState([]);
+//         const [selectedMovie, setSelectedMovie] = useState(null);
+      
+//         if (!registered) return (<RegistrationView registration={(register) => this.registration(register)}/>);
+
+      
+//         if (!user) {
+//           return (
+//             <LoginView
+//               onLoggedIn={(user, token) => {
+//                 setUser(user);
+//                 setToken(token);
+//               }}
+//             />
+//           );
+//         }
+      
+
+//         useEffect(() => {
+//               if (!token) {
+//                 return;
+//               }
+          
+//               fetch("https://enigmatic-river-99618.herokuapp.com/movies", {
+//                 headers: { Authorization: `Bearer ${token}` }
+//               })
+//                 .then((response) => response.json())
+//                 .then((movies) => {
+//                   console.log(movies);
+//                   setMovies(movies);
+//                 });
+//             }, [token]);
+      
+//         if (movies.length === 0) return <div className="main-view" />;
+
+//         return (
+//           <Row className="main-view justify-content-md-center">
+//             {selectedMovie
+//               ? (
+//                 <Col md={8}>
+//                   <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+//                 </Col>
+//               )
+//               : movies.map(movie => (
+//                 <Col md={3}>
+//                   <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+//                 </Col>
+//               ))
+//             }
+//           </Row>
+//         );
+//       }
+    
+//     }
+// {/* <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button> */}
+
+// export default MainView;
+
+
+
+
+import React from 'react';
+import axios from 'axios';
+import PropTypes from "prop-types";
+
+import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
 
 import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
-import { MovieCard } from "../movie-card/movie-card";
+import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from "../movie-view/movie-view";
+// import { Navbar } from "../navbar/navbar";
+// import { DirectorView } from "../director-view/director-view";
+// import { GenreView } from "../genre-view/genre-view";
+// import ProfileView from "../profile-view/profile-view";
 
-class MainView extends React.Component {
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-    constructor(){
-        super();
-        this.state = {
-          movies: [],
-          selectedMovie: null,
-        }
-      }
+import './main-view.scss';
 
-      componentDidMount(){
-        axios.get('https://enigmatic-river-99618.herokuapp.com/movies')
-          .then(response => {
-            this.setState({
-              movies: response.data
-            });
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      }
+export class MainView extends React.Component {
 
-      setSelectedMovie(newSelectedMovie) {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      favoriteMovies: [],
+      selectedMovie: null,
+      user: null,
+      registered: true,
+    };
+  }
+  componentDidMount() {
+    let accessToken = localStorage.getItem("token");
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem("user"),
+      });
+      this.getMovies(accessToken);
+    }
+  }
+
+  getMovies(token) {
+    axios
+      .get("https://myflix-firstmovieapp.herokuapp.com/movies", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        // Assign the result to the state
         this.setState({
-          selectedMovie: newSelectedMovie
+          movies: response.data,
         });
-      }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-      onLoggedIn(authData) {
-        console.log(authData);
-        this.setState({
-          user: authData.user.Username
-        });
-      
-        localStorage.setItem('token', authData.token);
-        localStorage.setItem('user', authData.user.Username);
-        this.getMovies(authData.token);
-      }
+/* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
 
-      registration(registered) {
-        this.setState({
-          registered,
-        });
-      }
+onLoggedIn(authData) {
+  console.log(authData);
+  this.setState({
+    user: authData.user.Username
+  });
 
-      getMovies(token) {
-        axios.get('https://enigmatic-river-99618.herokuapp.com/movies', {
-          headers: { Authorization: `Bearer ${token}`}
-        })
-        .then(response => {
-          // Assign the result to the state
-          this.setState({
-            movies: response.data
-          });
+  localStorage.setItem('token', authData.token);
+  localStorage.setItem('user', authData.user.Username);
+  this.getMovies(authData.token);
+}
+
+onLoggedOut() {
+  localStorage.removeItem('token');
+  localStorage.removeItem('user');
+  this.setState({
+    user: null
+  });
+}
+
+toRegister(registered) {
+  this.setState({
+    registered,
+  });
+}
+
+handleFavorite = (movieId, action) => {
+  const { user, favoriteMovies } = this.state;
+  const accessToken = localStorage.getItem("token");
+  const username = user;
+  if (accessToken !== null && username !== null) {
+    // Add MovieID to Favorites (local state & webserver)
+    if (action === "add") {
+      this.setState({ favoriteMovies: [...favoriteMovies, movieId] });
+      axios
+        .put(
+          `https://myflix-firstmovieapp.herokuapp.com/users/${username}/movies/${movieId}`,
+          {},
+          {
+            headers: { Authorization: `Bearer ${accessToken}` },
+          }
+        )
+        .then((res) => {
+          console.log(`Movie added to ${username} Favorite movies`);
+          alert(`Movie added to ${username} Favorite movies`);
         })
         .catch(function (error) {
           console.log(error);
         });
-      }
 
-      render() {
-        // const { movies, selectedMovie, registered } = this.state;
-        const storedUser = JSON.parse(localStorage.getItem("user"));
-        const storedToken = localStorage.getItem("token");
-        const [user, setUser] = useState(storedUser? storedUser : null);
-        const [token, setToken] = useState(storedToken? storedToken : null);
-        const [movies, setMovies] = useState([]);
-        const [selectedMovie, setSelectedMovie] = useState(null);
-      
-        if (!registered) return (<RegistrationView registration={(register) => this.registration(register)}/>);
-
-      
-        if (!user) {
-          return (
-            <LoginView
-              onLoggedIn={(user, token) => {
-                setUser(user);
-                setToken(token);
-              }}
-            />
-          );
-        }
-      
-
-        useEffect(() => {
-              if (!token) {
-                return;
-              }
-          
-              fetch("https://enigmatic-river-99618.herokuapp.com/movies", {
-                headers: { Authorization: `Bearer ${token}` }
-              })
-                .then((response) => response.json())
-                .then((movies) => {
-                  console.log(movies);
-                  setMovies(movies);
-                });
-            }, [token]);
-      
-        if (movies.length === 0) return <div className="main-view" />;
-
-        return (
-          <Row className="main-view justify-content-md-center">
-            {selectedMovie
-              ? (
-                <Col md={8}>
-                  <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-                </Col>
-              )
-              : movies.map(movie => (
-                <Col md={3}>
-                  <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-                </Col>
-              ))
-            }
-          </Row>
-        );
-      }
-    
+      // Remove MovieID from Favorites (local state & webserver)
+    } else if (action === "remove") {
+      this.setState({
+        favoriteMovies: favoriteMovies.filter((id) => id !== movieId),
+      });
+      axios
+        .delete(
+          `https://myflix-firstmovieapp.herokuapp.com/users/${username}/favorites/${movieId}`,
+          {
+            headers: { Authorization: `Bearer ${accessToken}` },
+          }
+        )
+        .then((res) => {
+          console.log(`Movie removed from ${username} Favorite movies`);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
-{/* <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button> */}
+  }
+};
+render() {
+  const { movies, user, favoriteMovies } = this.state;
+  return (
+    <BrowserRouter>
+    {/* <Routes> */}
+      {/* <Navbar user={user} /> */}
+      <Row className="main-view justify-content-md-center mt-3">
+        <Route
+          exact
+          path="/"
+          render={() => {
+            if (!user)
+              return (
+                <Col>
+                  <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+                </Col>
+              );
+            if (movies.length === 0) return <div className="main-view" />;
+            return movies.map((m) => (
+              <Col md={3} key={m._id}>
+                <MovieCard movie={m} />
+              </Col>
+            ));
+          }}
+        />
+        <Route
+          path="/register"
+          render={() => {
+            // if (user) return <Redirect to="/" />;
+            ()=><RegistrationView/>
+            return (
+              <Col>
+                <RegistrationView />
+              </Col>
+            );
+          }}
+        />
+
+        {/* route for link on main-view to profile-view */}
+
+        <Route
+            path={`/users/${user}`}
+            render={({ history }) => {
+              if (!user) return <Redirect to="/" />;
+              return (
+                <Col>
+                  <ProfileView 
+                  user={user} 
+                  goBack={history.goBack} 
+                  favoriteMovies={favoriteMovies || []} 
+                  handleFavorite={this.handleFavorite} 
+                  onBackClick={() => history.goBack()} 
+                  movies={movies} />
+                </Col>
+              );
+            }}
+          />
+
+          <Route
+            path={`/user-update/:username`}
+            render={({ match, history }) => {
+              if (!user) return <Redirect to="/" />;
+              return (
+                <Col>
+                  <UserUpdate
+                    user={user}
+                    onBackClick={() => history.goBack()}
+                  />
+                </Col>
+              );
+            }}
+          />
+
+
+        <Route
+          path="/movies/:movieId"
+          render={({ match, history }) => {
+            if (!user)
+              return (
+                <Col>
+                  <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+                </Col>
+              );
+            if (movies.length === 0) return <div className="main-view" />;
+            return (
+              <Col md={8}>
+                <MovieView
+                  movie={movies.find((m) => m._id === match.params.movieId)}
+                  onBackClick={() => history.goBack()}
+                  handleFavorite={this.handleFavorite}
+                />
+              </Col>
+            );
+          }}
+        />
+
+        <Route
+          path="/directors/:name"
+          render={({ match, history }) => {
+            if (!user)
+              return (
+                <Col>
+                  <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+                </Col>
+              );
+            if (movies.length === 0) return <div className="main-view" />;
+            return (
+              <Col md={8}>
+                <DirectorView
+                  director={
+                    movies.find((m) => m.Director.Name === match.params.name)
+                      .Director
+                  }
+                  onBackClick={() => history.goBack()}
+                />
+              </Col>
+            );
+          }}
+        />
+
+        <Route
+          path="/genres/:name"
+          render={({ match, history }) => {
+            if (!user)
+              return (
+                <Col>
+                  <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+                </Col>
+              );
+            if (movies.length === 0) return <div className="main-view" />;
+            return (
+              <Col md={8}>
+                <GenreView
+                  genre={
+                    movies.find((m) => m.Genre.Name === match.params.name)
+                      .Genre
+                  }
+                  onBackClick={() => history.goBack()}
+                />
+              </Col>
+            );
+          }}
+        />
+      </Row>
+    {/* </Routes> */}
+    </BrowserRouter>
+  );
+}
+}
 
 export default MainView;
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,42 +1,194 @@
-import './registration-view.scss';
+// import './registration-view.scss';
+
+// import React, {useState} from 'react';
+// import PropTypes from 'prop-types';
+
+// import { Form, Button, Card, CardGroup, Container, Col, Row, } from "react-bootstrap";
+
+// export function RegistrationView(props) {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [birthday, setBirthday] = useState("");
+  
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+
+//     const data = {
+//       Username: username,
+//       Password: password,
+//       Email: email,
+//       Birthday: birthday
+//     };
+
+//     fetch("https://enigmatic-river-99618.herokuapp.com/users", {
+//       method: "POST",
+//       body: JSON.stringify(data),
+//       headers: {
+//         "Content-Type": "application/json"
+//       }
+//     }).then((response) => {
+//       if (response.ok) {
+//         alert("Signup successful");
+//         window.location.reload();
+//       } else {
+//         alert("Signup failed");
+//       }
+//     });
+//   };
+
+//   return (
+//     <Container className="register-container">
+//       <Row className="justify-content-md-center mt-5">
+//         <Col md={5}>
+//           <CardGroup>
+//             <Card>
+//               <Card.Body>
+//                 <Card.Title className="text-center" as="h4">
+//                   Please Register for the myFlix movie app!
+//                 </Card.Title>
+//                 <Form>
+//                   <Form.Group>
+//                     <Form.Label>Username:</Form.Label>
+//                     <Form.Control
+//                       type="text"
+//                       value={username}
+//                       onChange={(e) => setUsername(e.target.value)}
+//                       minLength="3"
+//                     />
+//                   </Form.Group>
+
+//                   <Form.Group>
+//                     <Form.Label>Password:</Form.Label>
+//                     <Form.Control
+//                       type="text"
+//                       value={password}
+//                       onChange={(e) => setPassword(e.target.value)}
+//                       minLength="8"
+//                       placeholder="Minimum 8 characters"
+//                     />
+//                   </Form.Group>
+
+//                   <Form.Group>
+//                     <Form.Label>Email:</Form.Label>
+//                     <Form.Control
+//                       type="text"
+//                       value={email}
+//                       onChange={(e) => setEmail(e.target.value)}
+//                     />
+//                   </Form.Group>
+
+//                   <Form.Group>
+//                     <Form.Label>Birthday:</Form.Label>
+//                     <Form.Control
+//                       type="date"
+//                       value={birthday}
+//                       onChange={(e) => setBirthday(e.target.value)}
+//                     />
+//                   </Form.Group>
+
+//                   <Button
+//                     className="sign-up-button mt-2 mr-2"
+//                     variant="primary"
+//                     type="submit"
+//                     onClick={handleSubmit}
+//                   >Register
+//                   </Button>
+               
+//                 </Form>
+//               </Card.Body>
+//             </Card>
+//           </CardGroup>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// }
+
+// RegistrationView.propTypes = {
+//   // movie: PropTypes.shape({
+//   //   // shape({..}) means that its an object
+//   //   Title: PropTypes.string.isRequired,
+//   //   Description: PropTypes.string.isRequired,
+//   //   ImagePath: PropTypes.string.isRequired,
+//   //   Genre: PropTypes.shape({
+//   //     Name: PropTypes.string.isRequired,
+//   //   }),
+//   // }).isRequired,
+//   registration: PropTypes.func.isRequired,
+// };
+
+
 
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 import { Form, Button, Card, CardGroup, Container, Col, Row, } from "react-bootstrap";
+
+import './registration-view.scss';
 
 export function RegistrationView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [birthday, setBirthday] = useState("");
-  
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const [usernameErr, setUsernameErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
+  const [emailErr, setEmailErr] = useState("");
 
-    const data = {
-      Username: username,
-      Password: password,
-      Email: email,
-      Birthday: birthday
-    };
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setUsernameErr("Username Required");
+      isReq = false;
+    } else if (username.length < 2) {
+      setUsernameErr("Username must be 2 characters long");
+      isReq = false;
+    }
+    if (!password) {
+      setPasswordErr("Password Required");
+      isReq = false;
+    } else if (password.length < 6) {
+      setPasswordErr("Password must be 6 characters long");
+      isReq = false;
+    }
+    if (!email) {
+      setEmailErr("Email Required");
+      isReq = false;
+    } else if (email.indexOf("@") === -1) {
+      setEmailErr("Email is invalid");
+      isReq = false;
+    }
 
-    fetch("https://enigmatic-river-99618.herokuapp.com/users", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }).then((response) => {
-      if (response.ok) {
-        alert("Signup successful");
-        window.location.reload();
-      } else {
-        alert("Signup failed");
-      }
-    });
+    return isReq;
   };
-
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //console.log(username, password, email, birthday);
+   // props.Registration(username);
+   const isReq = validate();
+   if (isReq) {
+     axios
+       .post("https://myflix-firstmovieapp.herokuapp.com/users", {
+         Username: username,
+         Password: password,
+         Email: email,
+         Birthday: birthday,
+       })
+       .then((response) => {
+         const data = response.data;
+         console.log(data);
+         alert("Registration successful, please login!");
+         window.open("/", "_self"); //the second argument '_self' makes the page open in the current tab
+       })
+       .catch((response) => {
+         console.error(response);
+         alert("unable to register");
+       });
+   }
+  };
   return (
     <Container className="register-container">
       <Row className="justify-content-md-center mt-5">
@@ -45,7 +197,7 @@ export function RegistrationView(props) {
             <Card>
               <Card.Body>
                 <Card.Title className="text-center" as="h4">
-                  Please Register for the myFlix movie app!
+                  Please Register
                 </Card.Title>
                 <Form>
                   <Form.Group>
@@ -54,8 +206,10 @@ export function RegistrationView(props) {
                       type="text"
                       value={username}
                       onChange={(e) => setUsername(e.target.value)}
-                      minLength="3"
+                      //required
+                      placeholder="Enter a username"
                     />
+                     {usernameErr && <p>{usernameErr}</p>}
                   </Form.Group>
 
                   <Form.Group>
@@ -64,9 +218,11 @@ export function RegistrationView(props) {
                       type="text"
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
+                      //required
                       minLength="8"
-                      placeholder="Minimum 8 characters"
+                      placeholder="Must be 8 or more characters"
                     />
+                    {passwordErr && <p>{passwordErr}</p>}
                   </Form.Group>
 
                   <Form.Group>
@@ -75,7 +231,10 @@ export function RegistrationView(props) {
                       type="text"
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
+                      //required
+                      placeholder="Enter your email address"
                     />
+                     {emailErr && <p>{emailErr}</p>}
                   </Form.Group>
 
                   <Form.Group>
@@ -84,6 +243,7 @@ export function RegistrationView(props) {
                       type="date"
                       value={birthday}
                       onChange={(e) => setBirthday(e.target.value)}
+                      placeholder="Enter your birthday"
                     />
                   </Form.Group>
 
@@ -94,7 +254,11 @@ export function RegistrationView(props) {
                     onClick={handleSubmit}
                   >Register
                   </Button>
-               
+                  <Button 
+                  className="back-button mt-2"
+                  variant="secondary"
+                  type="submit"
+                  onClick={() => { onBackClick(null); }}>Back to the Login Page</Button>
                 </Form>
               </Card.Body>
             </Card>
@@ -105,15 +269,9 @@ export function RegistrationView(props) {
   );
 }
 
-RegistrationView.propTypes = {
-  // movie: PropTypes.shape({
-  //   // shape({..}) means that its an object
-  //   Title: PropTypes.string.isRequired,
-  //   Description: PropTypes.string.isRequired,
-  //   ImagePath: PropTypes.string.isRequired,
-  //   Genre: PropTypes.shape({
-  //     Name: PropTypes.string.isRequired,
-  //   }),
-  // }).isRequired,
-  registration: PropTypes.func.isRequired,
+RegistrationView.propTypes = {register: PropTypes.shape({
+  Username: PropTypes.string.isRequired,
+  Password: PropTypes.string.isRequired,
+  Email: PropTypes.string.isRequired,
+}),
 };
